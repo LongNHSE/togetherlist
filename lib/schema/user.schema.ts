ecf8f8857208ae2b060703971c4084a09ff0db38
Nguyen Huy Long <huylong2113@gmail.com>
2024-05-15 01:16:35 +0700

    Add task card UI [LongNH]

---
+++ lib/schema/user.schema.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const UserSchema = z.object({
+  _id: z.string().optional(),
+  firstName: z.string(),
+  lastName: z.string(),
+  username: z.string(),
+  email: z.string(),
+  password: z.string().optional(),
+  role: z.enum(['admin', 'user', 'staff']).default('user').optional(),
+  status: z.enum(['active', 'inactive']).default('active').optional(),
+  avatar: z.string().optional(),
+  phone: z.string().optional(),
+  gender: z.string().optional(),
+  refreshToken: z.string().optional(),
+});
+
+export type UserType = z.infer<typeof UserSchema>;
