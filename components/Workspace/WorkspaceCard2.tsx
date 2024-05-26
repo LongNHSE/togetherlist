281ca7574434064b4f2629edd79a4e88e2b6c7dc
Nguyen Huy Long <huylong2113@gmail.com>
2024-05-26 16:01:59 +0700

    Fix conflict [LongNH]

--- components/Workspace/WorkspaceCard2.tsx
+++ components/Workspace/WorkspaceCard2.tsx
@@ -18,6 +18,13 @@ import ProgressTasks from './ProgressTasks';
 import { BoardType } from '@/lib/schema/board/board.schema';
 
 const WorkspaceCard = ({ board }: { board: BoardType }) => {
+  const statuses = [
+    { value: 20, colorClass: 'bg-red-500', label: 'Status 1' },
+    { value: 30, colorClass: 'bg-blue-500', label: 'Status 2' },
+    { value: 20, colorClass: 'bg-gray-500', label: 'Status 2' },
+    { value: 20, colorClass: 'bg-green-500', label: 'Status 3' },
+    { value: 10, colorClass: 'bg-pink-500', label: 'Status 4' },
+  ];
   return (
     <div>
       {/* <Card className="max-w-[25rem] h-auto flex flex-col space-y-5 p-5 bg-white shadow-lg rounded-xl select-none"> */}
@@ -50,7 +57,7 @@ const WorkspaceCard = ({ board }: { board: BoardType }) => {
         <CardContent className="flex flex-col gap-2">
           {/* Progress */}
           <div className="flex items-center gap-3">
-            <ProgressTasks
+            {/* <ProgressTasks
               width="w-[40%]"
               idLabel="Not Done"
               labelValue="Not Done"
@@ -72,6 +79,12 @@ const WorkspaceCard = ({ board }: { board: BoardType }) => {
               labelValue="Done"
               progressValue={50}
               progressColorClass="bg-green-500"
+            /> */}
+            <ProgressTasks
+              width="w-full"
+              idLabel="example-progress"
+              labelValue="Progress"
+              statuses={statuses}
             />
           </div>
         </CardContent>
